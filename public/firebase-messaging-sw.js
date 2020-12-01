533baac2065220a7ae6a1dd0174a3645382c877a
pradytia <pradytia@mobilecom.co.id>
2020-12-01 21:19:39 +0700

    change setting firebase

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -11,12 +11,9 @@ importScripts('https://www.gstatic.com/firebasejs/8.1.1/firebase-messaging.js');
 //     }
 
 firebase.initializeApp({
-    apiKey: "AIzaSyCVrxJnSbqY9QuBGbPdfPtTqOzyw553egQ",
-    authDomain: "fir-push-notif-6d290.firebaseapp.com",
-    databaseURL: "https://fir-push-notif-6d290.firebaseio.com",
-    projectId: "fir-push-notif-6d290",
-    storageBucket: "fir-push-notif-6d290.appspot.com",
     messagingSenderId: "115992207732",
+    projectId: "fir-push-notif-6d290",
+    apiKey: "AIzaSyCVrxJnSbqY9QuBGbPdfPtTqOzyw553egQ",
     appId: "1:115992207732:web:11bbf881e9229fdb5b0c21"
 });
 
